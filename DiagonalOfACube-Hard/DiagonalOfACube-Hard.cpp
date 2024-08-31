560c6c81fbda4e9670ac621c851a29570818dcda
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-31 23:59:15 +0300

    Add problem's solution

---
+++ DiagonalOfACube-Hard/DiagonalOfACube-Hard.cpp
@@ -0,0 +1,30 @@
+//Author : Abdulhamit Sahadi
+//Created: Aug 31, 2024
+//Problem Link: https://edabit.com/challenge/uEiTAhNN6HgESspWb
+
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+double cubeDiagonal(double volume) {
+
+	double side = pow(volume, 1 / 3.0);
+	return side * pow(3, 1 / 2.0);
+}
+
+double readDouble() {
+
+	double number = 0.0;
+	cin >> number;
+	return number;
+}
+
+int main() {
+
+	double volume = readDouble();
+
+	cout << fixed << setprecision(2) << cubeDiagonal(volume) << endl;
+
+	return 0;
+}
